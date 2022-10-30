ec9aa25e0c1132afa0691e027c2d60aeae746b7d
Lawrence Fritts <lawrencefritts@icloud.com>
2022-10-29 20:56:46 -0700

    added admin to cannibas

--- rstuff/cannibas/admin.py
+++ rstuff/cannibas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from ./models import Producer, StoreCannibas, Seed, Plant, FlowerRank
 
+admin.site.register(Producer)
+admin.site.register(StoreCannibus)
+admin.site.register(Seed)
+admin.site.register(Plant)
+admin.site.register(FlowerRank)
